7b99e201b007d4ba0681fd0e0901cf0107afc0e9
David Cato <git@crunchyfrog.net>
2010-06-25 18:57:51 -0700

    Update tabs for Radiant 0.9

--- ad_banners_extension.rb
+++ ad_banners_extension.rb
@@ -5,16 +5,15 @@ class AdBannersExtension < Radiant::Extension
   version "0.1"
   description "Manage ad banners"
   url "http://github.com/davec/radiant-ad_banners-extension"
-  
+ 
   def activate
     Radiant::AdminUI.send :include, AdBannersAdminUI unless defined? admin.ad_banner
     admin.ad_banner = Radiant::AdminUI.load_default_ad_banner_regions
 
-    admin.tabs.add "Ads", "/admin/ad_banners", :after => "Layouts", :visibility => [:all]
+    tab "Content" do
+      add_item "Ads", "/admin/ad_banners", :after => "Pages"
+    end
     Page.send :include, AdBannerTags
   end
-  
-  def deactivate
-  end
-  
+ 
 end
