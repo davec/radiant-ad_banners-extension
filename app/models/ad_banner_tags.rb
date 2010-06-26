2f363d4c645fe52d98d1b6bd701251a9851b9b81
David Cato <git@crunchyfrog.net>
2010-06-25 19:23:44 -0700

    Don't generate target attr if link_target is blank

--- app/models/ad_banner_tags.rb
+++ app/models/ad_banner_tags.rb
@@ -20,13 +20,13 @@ module AdBannerTags
       @selected_banners << ad_banner.id
       # The HTML is simple enough to roll by hand instead of sucking in REXML
       returning String.new do |result|
-        unless ad_banner.link_url.blank?
+        if ad_banner.link_url.present?
           result << %Q{<a href="#{CGI.escapeHTML(ad_banner.link_url)}"}
-          result << %Q{ target="#{ad_banner.link_target}"}
+          result << %Q{ target="#{ad_banner.link_target}"} if ad_banner.link_target.present?
           result << '>'
         end
         result << %Q{<img src="#{ad_banner.asset.thumbnail(:original)}" title="#{ad_banner.name}" alt="#{ad_banner.asset.caption || ad_banner.asset.title}" />}
-        result << '</a>' unless ad_banner.link_url.blank?
+        result << '</a>' if ad_banner.link_url.present?
       end
     end
   end
